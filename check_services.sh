aeb8b5fc8ee1dbb06681e57133f381a632a010fc
Aboulkacem KRADRA <kradra.belkacem@gmail.com>
2024-01-07 09:00:07 +0100

    Update check_services.sh
    added more comments

--- check_services.sh
+++ check_services.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+#
+# Essential Services Status Check Script
+# Author: Aboulkacem KRADRA (@kacemsys)
+#
+# Description:
+#   This script checks and displays the status of essential services on the system.
+#   The list of essential services is configurable, and the script uses systemctl
+#   to check the status of each service. The results include the service name and
+#   its current status.
+#
 
 # Configurable list of essential services
 declare -a services=("sshd" "httpd" "firewalld")
@@ -11,7 +21,7 @@ check_service_status() {
 }
 
 # Check the status of essential services
-echo "Services status :$(date)" 
+echo "Services status : $(date)" 
 for service in "${services[@]}"; do
   check_service_status "$service" 
 done
