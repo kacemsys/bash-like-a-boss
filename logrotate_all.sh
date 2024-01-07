068742fcb7a930ad5e6c3f9b0ca31694ca8e61ed
Aboulkacem KRADRA <kradra.belkacem@gmail.com>
2024-01-07 08:57:24 +0100

    Update logrotate_all.sh
    added more comment

--- logrotate_all.sh
+++ logrotate_all.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+#
+# Log Rotation Script
+# Author: Aboulkacem KRADRA (@kacemsys)
+#
+# Description:
+#   This script rotates and compresses old log files in the /var/log directory using
+#   the logrotate utility. It rotates logs daily and keeps 4 weeks worth, providing
+#   debug and verbose output during the rotation process.
+#
 
 # Rotate and compress old log files in /var/log
 for f in /var/log/*.log; do
@@ -6,5 +15,5 @@ for f in /var/log/*.log; do
   logrotate -d -v -f /etc/logrotate.conf "$f"
 done
 
+# Inform the user that log files have been rotated and compressed
 echo "Log files rotated and compressed!"
-
